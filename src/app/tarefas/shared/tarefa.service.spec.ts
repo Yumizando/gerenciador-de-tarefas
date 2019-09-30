c2ba18bfdd3cde28d072b9451f3cf1ddf45f4745
Laryssa Yumi <laryyumi@gmail.com>
2019-09-30 18:26:02 -0300

    Criado componente listar-tarefas

--- src/app/tarefas/shared/tarefa.service.spec.ts
+++ src/app/tarefas/shared/tarefa.service.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
+import { inject } from '@angular/core/testing';
 
 import { TarefaService } from './tarefa.service';
+import { Tarefa } from './tarefa.model';
 
 describe('TarefaService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
@@ -9,4 +11,14 @@ describe('TarefaService', () => {
     const service: TarefaService = TestBed.get(TarefaService);
     expect(service).toBeTruthy();
   });
+
+  /*it('deve garantir que tarefa1 seja cadastrada',
+    inject([TarefaService], (service: TarefaService) =>{
+      let tarefa = new Tarefa(1, 'tarefa1', false);
+      service.cadastrar(tarefa);
+      let tarefa1 = service.buscarPorId(1);
+      expect(tarefa1.id).toBeGreaterThan(0);
+    })
+  );*/
+
 });
